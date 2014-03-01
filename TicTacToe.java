609a9dcb1eb4b842ef43621d558ba1dc83956a98
Archos67 <ghyzel.chris@gmail.com>
2014-03-01 11:22:54 -0500

    Adding TicTacToe Gui framework

---
+++ TicTacToe.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+public class TicTacToe implements ActionListener {
+	/* Declare global variables */
+	//the JFrame window
+
+	//JMenuBar
+
+	//JMenuItems
+
+	//JButtons ex: back
+
+	//Board button array 
+
+	//JPanels
+	
+	//JLabel
+	
+	//JTextArea for messages
+	
+	/* Ways to win on a
+	 * Tic Tac Toe Board
+	 * 0 1 2
+	 * 3 4 5
+	 * 6 7 8
+	 */
+	//Size of your game screen X by Y
+
+	/* Flags */
+
+	/* Message to display */
+
+	/* Turn Number */
+
+	/* use this to call the class constructor*/
+	public static void main(String[] args) {
+
+	}
+	/* Set up the game! */
+	public TicTacToe() {
+		/* Set Properties of window HINT: USE WINDOW API */
+		/* Panel Properties Background color, layout HINT: USE FlowLayout or GridLayout*/
+
+
+		/* Add Action Listener to all the Buttons and Menu Items */
+
+		/* Set up a 3x3 playing field */
+		/*Set initial displays in main panels */
+		/*Set Panel locations*/
+		/*set visible to true */
+	}
+
+	//What happens when someone clicks?
+	public void actionPerformed(ActionEvent click) {
+		// get source of click
+
+		/* Check if it was any of your game buttons, if so update and disable them */
+
+		/* if a button was clicked, check for a win */
+
+		/* If they select the new game menu? */
+
+		/* If they select to start a new game? */
+
+		/* What if they are already in a game (or not) */
+
+		/*If they want to exit*/
+
+		/* Other options? */
+
+		/* If they go back from new g ame menu? */
+		/* are they ingame or not? */
+
+	}
+	/*Clear other panels, show the game board */
+	public void showGame() {
+	}
+	/* check win conditions */
+	public void checkWin() { 
+		/* If someone wins, who is it and then what? */
+
+	}
+	/* Clears the active panels */
+	public void clearActivePanels() {
+	}
+}
